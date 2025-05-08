47e5cda1345f8f6ef35e9fcab280eb00dc67b421
Timur Kramár <timurjan.kramar123@gmail.com>
2025-05-08 21:01:41 +0200

    Add example with KeyboardGestureArea bug

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
 import CustomKeyboardAwareScrollView from "@/components/CustomKeyboardAwareScrollView";
 import { useState } from "react";
 import { Text, TextInput, View } from "react-native";
-import { KeyboardStickyView } from "react-native-keyboard-controller";
+import {
+  KeyboardAwareScrollView,
+  KeyboardGestureArea,
+  KeyboardStickyView,
+} from "react-native-keyboard-controller";
 
-export default function Index() {
+function Example1() {
   const [text, setText] = useState("");
   return (
     <View
@@ -48,3 +52,66 @@ export default function Index() {
     </View>
   );
 }
+
+function Example2() {
+  const [text, setText] = useState("");
+  return (
+    <KeyboardGestureArea
+      style={{ flex: 1 }}
+      textInputNativeID="textInput"
+      offset={80}
+    >
+      <KeyboardAwareScrollView
+        keyboardDismissMode="interactive"
+        style={{ borderWidth: 1, borderColor: "red" }}
+      >
+        {Array.from({ length: 20 }).map((_, index) => (
+          <Text key={index}>Bla bla bla</Text>
+        ))}
+        <TextInput
+          placeholder="Enter your text"
+          style={{
+            borderWidth: 1,
+            padding: 10,
+            borderRadius: 5,
+          }}
+          multiline={true}
+          scrollEnabled={false}
+          value={text}
+          onChangeText={setText}
+          nativeID="textInput"
+        />
+        <TextInput
+          placeholder="Enter your text"
+          style={{
+            borderWidth: 1,
+            padding: 10,
+            borderRadius: 5,
+          }}
+          multiline={true}
+          scrollEnabled={false}
+          value={text}
+          onChangeText={setText}
+          nativeID="textInput"
+        />
+        {Array.from({ length: 20 }).map((_, index) => (
+          <Text key={index}>Bla bla bla</Text>
+        ))}
+      </KeyboardAwareScrollView>
+      <KeyboardStickyView>
+        <View
+          style={{
+            borderWidth: 1,
+            padding: 10,
+            borderRadius: 5,
+            height: 80,
+          }}
+        >
+          <Text>Testing Sticky Toolbar</Text>
+        </View>
+      </KeyboardStickyView>
+    </KeyboardGestureArea>
+  );
+}
+
+export default Example2;
